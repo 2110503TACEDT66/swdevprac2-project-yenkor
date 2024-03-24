576b4b24c48ad6ba717d451b708735e0b4a88b55
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-03-25 01:46:11 +0700

    Fix overflow and sticky issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,28 +27,13 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="h-[200vh] overflow-hidden">
-      {/* Background */}
-      <div className="relative">
-        <Image
-          className={`absolute z-0 h-[100vh] ${
-            isSticky || isFirst
-              ? "top-[-100vh] right-[-16rem] transition-[top,right] duration-1000 ease-in-out"
-              : "top-0 right-64 transition-[top,right] duration-1000 ease-in-out"
-          }`}
-          alt="background"
-          src="/img/home_background.png"
-          width={1000}
-          height={1200}
-        />
-      </div>
-
-      <div className="py-12  sticky top-[-3rem] ">
+    <main className="h-[200vh]">
+      <div className="py-12  sticky top-[-3rem] z-50">
         <div
           className={`flex flex-row  justify-between items-center px-12 h-20 ${
             isSticky
               ? "top-0 bg-white transition duration-300 ease-in-out shadow-xl"
-              : "top-[-2rem] transition duration-300 ease-in-out z-50"
+              : "top-[-2rem] transition duration-300 ease-in-out"
           }`}
         >
           <Image
@@ -60,6 +45,7 @@ export default function Home() {
             }`}
             width={255}
             height={55}
+            className="z-50"
           />
           <div className="flex flex-row space-x-4">
             <button
@@ -80,47 +66,67 @@ export default function Home() {
         </div>
       </div>
 
-      {/*  Discover your rental car */}
-      <div className="flex flex-col items-center py-8 relative">
-        <div className="flex flex-col items-start  ">
-          <h1 className="text-3xl font-kiona text-white ml-3">DISCOVER YOUR</h1>
-          <h1 className="text-9xl font-kiona text-white">RENTAL CAR</h1>
-          <div
-            className={`flex flex-row w-full justify-center h-[10vh] ${
-              isSticky || isFirst
-                ? "pl-[200vw] transition-transform-[padding]  duration-1000 ease-in-out"
-                : "transition-transform-[padding]  duration-1000 ease-in-out"
-            }`}
-          >
+      {/* Background */}
+      <div className="overflow-hidden">
+        <Image
+          className={`absolute  z-0 h-[100vh]  ${
+            isSticky || isFirst
+              ? "top-[-100vh] right-[-16rem] transition-[top,right] duration-1000 ease-in-out"
+              : "top-0 right-64 transition-[top,right] duration-1000 ease-in-out"
+          }`}
+          alt="background"
+          src="/img/home_background.png"
+          width={1000}
+          height={1200}
+        />
+
+        {/*  Discover your rental car */}
+        <div>
+          <div className="flex flex-col items-center py-8 relative">
+            <div className="flex flex-col items-start  ">
+              <h1 className="text-3xl font-kiona text-white ml-3">
+                DISCOVER YOUR
+              </h1>
+              <h1 className="text-9xl font-kiona text-white">RENTAL CAR</h1>
+              <div
+                className={`flex flex-row w-full justify-center h-[10vh] ${
+                  isSticky || isFirst
+                    ? "pl-[200vw] transition-transform-[padding]  duration-1000 ease-in-out"
+                    : "transition-transform-[padding]  duration-1000 ease-in-out"
+                }`}
+              >
+                <Image
+                  className="absolute top-[-100px] w-[65vw] mt-3"
+                  alt="car"
+                  src="/img/porsche-model.png"
+                  width={1000}
+                  height={1000}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Footer */}
+
+        <div className="flex flex-row p-12 justify-between items-center pt-[22rem] z-30">
+          <div className="flex flex-row pl-5 space-x-5 items-center z-0">
             <Image
-              className="absolute top-[-100px] w-[65vw] mt-3"
-              alt="car"
-              src="/img/porsche-model.png"
-              width={1000}
-              height={1000}
+              alt="logo"
+              src="/img/explore_icon.png"
+              width={50}
+              height={50}
+              className="z-0"
             />
+            <div className="flex flex-col">
+              <h1 className="text-white text-lg">Explore the </h1>
+              <h1 className="text-white text-lg">new travel experience</h1>
+            </div>
           </div>
-        </div>
-      </div>
 
-      {/* Footer */}
-
-      <div className="flex flex-row p-12 justify-between items-center pt-[22rem] z-50">
-        <div className="flex flex-row pl-5 space-x-5 items-center z-50">
-          <Image
-            alt="logo"
-            src="/img/explore_icon.png"
-            width={50}
-            height={50}
-          />
-          <div className="flex flex-col">
-            <h1 className="text-white text-lg">Explore the </h1>
-            <h1 className="text-white text-lg">new travel experience</h1>
+          <div className="flex flex-row space-x-4">
+            <Image alt="dec" src="/img/dec_1.png" width={100} height={100} />
           </div>
-        </div>
-
-        <div className="flex flex-row space-x-4">
-          <Image alt="dec" src="/img/dec_1.png" width={100} height={100} />
         </div>
       </div>
     </main>
