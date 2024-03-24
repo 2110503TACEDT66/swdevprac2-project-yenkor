f220aabc5fd047690106ad26beda831897f26ab8
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-03-25 00:50:26 +0700

    Add sticky header and add transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,34 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isSticky, setIsSticky] = useState(false);
+  const [isFirst, setIsFirst] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    setTimeout(() => {
+      setIsFirst(false);
+    }, 100);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <main className="w-[100vw] h-[100vh]">
+    <main className="h-[200vh]">
+      {/* Background */}
       <Image
         className="absolute top-0 right-64 z-0 h-[100vh]"
         alt="background"
@@ -10,37 +36,56 @@ export default function Home() {
         width={1000}
         height={1200}
       />
-      <div className="flex flex-row p-12 justify-between items-center">
-        <Image
-          alt="logo"
-          src="/img/yenkor_logo_trans.png"
-          width={255}
-          height={55}
-        />
 
-        <div className="flex flex-row space-x-4">
-          <button
-            className="bg-[#4E5861] px-6 py-2 rounded-md font-bold text-white
-          hover:bg-[#2D2D2D] transition duration-300 ease-in-out
-          active:bg-[#2D2D2D] active:scale-95 active:shadow-inner"
-          >
-            Get Start
-          </button>
-          <button
-            className="bg-white px-6 py-2 rounded-md font-bold
-          hover:bg-[#F2F2F2] transition duration-300 ease-in-out
-          active:bg-[#F2F2F2] active:scale-95 active:shadow-inner"
-          >
-            Sign in
-          </button>
+      <div className="py-12  sticky top-[-3rem] ">
+        <div
+          className={`flex flex-row  justify-between items-center px-12 h-20 ${
+            isSticky
+              ? "top-0 bg-white transition duration-300 ease-in-out shadow-xl"
+              : "top-[-2rem] transition duration-300 ease-in-out z-50"
+          }`}
+        >
+          <Image
+            alt="logo"
+            src={`${
+              isSticky
+                ? "/img/yenkor_logo_black.png"
+                : "/img/yenkor_logo_trans.png"
+            }`}
+            width={255}
+            height={55}
+          />
+          <div className="flex flex-row space-x-4">
+            <button
+              className="bg-[#4E5861] px-6 py-2 rounded-md font-bold text-white shadow-lg
+              hover:bg-[#2D2D2D] transition duration-300 ease-in-out
+              active:bg-[#2D2D2D] active:scale-95 active:shadow-inner"
+            >
+              Get Start
+            </button>
+            <button
+              className="bg-white px-6 py-2 rounded-md font-bold shadow-lg
+              hover:bg-[#F2F2F2] transition duration-300 ease-in-out
+              active:bg-[#F2F2F2] active:scale-95 active:shadow-inner"
+            >
+              Sign in
+            </button>
+          </div>
         </div>
       </div>
 
+      {/*  Discover your rental car */}
       <div className="flex flex-col items-center py-8 relative">
         <div className="flex flex-col items-start  ">
-          <h1 className="text-3xl font-kiona text-white">DISCOVER YOUR</h1>
+          <h1 className="text-3xl font-kiona text-white ml-3">DISCOVER YOUR</h1>
           <h1 className="text-9xl font-kiona text-white">RENTAL CAR</h1>
-          <div className="flex flex-row w-full justify-center h-[10vh]">
+          <div
+            className={`flex flex-row w-full justify-center h-[10vh] ${
+              isSticky || isFirst
+                ? "pl-[200vw] transition-transform-[padding]  duration-1000 ease-in-out"
+                : "transition-transform-[padding]  duration-1000 ease-in-out"
+            }`}
+          >
             <Image
               className="absolute top-[-100px] w-[65vw] mt-3"
               alt="car"
@@ -51,6 +96,8 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* Footer */}
 
       <div className="flex flex-row p-12 justify-between items-center pt-[22rem] z-50">
         <div className="flex flex-row pl-5 space-x-5 items-center z-50">
