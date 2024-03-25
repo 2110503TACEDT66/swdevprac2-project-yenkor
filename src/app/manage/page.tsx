8298a5b821ba06e5912729d7cc21416b68188ed3
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-03-26 04:05:56 +0700

    Add ExploreCard and ManageCard components

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,8 +1,33 @@
 "use client";
 
+import ExploreCard from "@/components/ExploreCard";
 import NavBar from "@/components/NavBar";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
 import React, { useEffect, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import Image from "next/image";
+import ManageCard from "@/components/ManageCard";
 
 const page = () => {
   const [isSticky, setIsSticky] = useState(false);
@@ -24,36 +49,31 @@ const page = () => {
 
   const mockData = new Map();
   mockData.set("1", {
+    id: "1",
     name: "Lamborghini",
     location: "Bangkok 10400",
     phone: "0987654321",
+    rentDate: new Date(2024, 1, 1),
+    returnDate: new Date(2024, 1, 2),
   });
+
   return (
     <main>
       <NavBar stickyState={isSticky} showSignIn={false} />
       <div className="flex flex-col items-center">
         <div className=" rounded-xl w-[90vw] h-[80vh] flex flex-col justify-around items-center">
           <div className="w-full flex flex-row justify-end py-1 text-black text-2xl font-kiona relative">
-            <h1 className="bg-white rounded-lg px-4 absolute top-0">1/3</h1>
+            <h1 className="bg-white rounded-lg px-4 absolute top-0 z-20">
+              1/3
+            </h1>
           </div>
-          <div className="bg-black w-full h-[31%]">
-            {/* Card */}
-            <div className="flex flex-row w-full h-full bg-slate-500 rounded-lg shadow-lg">
-              <div className="w-[30%] h-full bg-red-100 relative rounded-lg">
-                <Image
-                  src="/img/place_holder.jpg"
-                  fill={true}
-                  alt="car"
-                  className="object-cover rounded-l-lg shadow-xl"
-                />
-              </div>
-              <div className="p-6 bg-orange-600">
-                <h1 className="text-white font-poppins font-bold text-4xl">
-                  {mockData.get("1").name}
-                </h1>
-              </div>
-            </div>
-            {/* Card */}
+          <div className="w-full h-[31%]">
+            <ManageCard
+              id={mockData.get("1").id}
+              name={mockData.get("1").name}
+              rentDate={mockData.get("1").rentDate}
+              returnDate={mockData.get("1").returnDate}
+            />
           </div>
           <div className="bg-black w-full h-[31%]"></div>
           <div className="bg-black w-full h-[31%]"></div>
