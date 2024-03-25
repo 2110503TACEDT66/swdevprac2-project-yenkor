d488ca8dc8c503b3be482531aecd08f3c3a9573a
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-03-25 18:18:38 +0700

    Add ExploreCard component

---
+++ src/components/ExploreCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+
+const ExploreCard = ({ src }: { src?: string }) => {
+  return (
+    <div className="w-[24%] h-[30rem] m-2 rounded-lg relative hover:scale-[102%] transition duration-200 ease-in-out active:scale-100">
+      <div className="w-full h-[75%] relative">
+        <Image
+          alt="image"
+          fill={true}
+          src={src || "/img/place_holder.jpg"}
+          className="object-cover rounded-lg"
+        />
+      </div>
+      <div className="bg-white rounded-b-lg w-full h-[30%] absolute bottom-0 flew flex-col">
+        <div className="text-black pt-5 pl-5">
+          <h1 className="text-2xl font-poppins font-bold">Lamborghini</h1>
+        </div>
+        <div className="text-black pt-2 pl-5">
+          <h1 className="text-lg font-poppins font-normal">
+            Yenkor Rental Car | Bangkok 10400 | 0987654321
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExploreCard;
