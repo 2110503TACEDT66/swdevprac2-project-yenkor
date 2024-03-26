3d37743b2feb6712cab21f4e4679e9a4d91f1b40
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-03-27 00:35:10 +0700

    Update import path for userLogIn in auth.ts

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,6 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import userLogIn from "./userLogIn";
+import userLogIn from "@/libs/userLogIn";
 
 export const authOptions: NextAuthOptions = {
   providers: [
