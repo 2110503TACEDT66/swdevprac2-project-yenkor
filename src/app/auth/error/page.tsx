04e6442b516c81c2a9b4af64945e755e51c5ae7b
Nithinan_ <nwattanasiri47@gmail.com>
2024-03-26 09:18:21 +0700

    Remove unrelated div

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -3,9 +3,6 @@ import LoginPage from "../login/page";
 export default function ErrorPage() {
     return (
         <main>
-            <div className="font-white">
-                Invalid Credentials
-            </div>
             <LoginPage errorMessage="Invalid Credentials"/>
         </main>
         
